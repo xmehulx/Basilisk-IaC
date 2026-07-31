f1a6b813f9311de8f0476b3ef018b717fefaf336
Mehul Singh <singhmehul@hotmail.com>
2026-07-31 03:18:13 +0000

    stat: v0.1a

--- scripts/tor_lxc/tor-stats.py
+++ scripts/tor_lxc/tor-stats.py
@@ -1,28 +1,39 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+from stem.control import Controller
 
-import subprocess
-import json
-import ast
+controller = Controller.from_port(port=9051)
+controller.authenticate()  # Use cookie or auth automatically
 
-def tor_stats():
-    result = subprocess.run(
-        ["ssh", "-i", "/home/infra/.ssh/darshak", "{}@{}".format(user, host_tor), "python3", "/home/darshak/code.py"],
-        capture_output=True,
-        text=True,
-        #check=True,
-    )
 
-    tor_stats = ast.literal_eval(result.stdout)
-    #print("Return code:", result.returncode)
-    #print("STDOUT:")
-    print(json.dumps(tor_stats))
-    #print("STDERR:")
-    #print(result.stderr)
+def get_bw_utilized(controller):
+    bytes_read = int(controller.get_info("traffic/read"))
+    bytes_written = int(controller.get_info("traffic/written"))
+
+    # Convert to GB
+    gb_read = round(bytes_read / (1024**3), 2)
+    gb_written = round(bytes_written / (1024**3), 2)
+    bandwidth = [gb_read, gb_written]
+    return bandwidth
+
+def get_conn(controller):
+    # Total active connections (circuits/OR connections)
+    connections = len(controller.get_circuits())
+    return connections
+
+def get_uptime(controller):
+    # Get uptime in seconds
+    uptime = controller.get_info("uptime")
+    return uptime
 
 if __name__ == "__main__":
-    user = "darshak"
-    host_tor = "10.0.0.14"
-    host_debian = "10.0.0.30"
-    host_wireguard = "10.0.0.213"
+    statistics = {}
+    bw_used = get_bw_utilized(controller)
+    total_conn = get_conn(controller)
+    uptime = get_uptime(controller)
+    statistics["BW_used"] = bw_used
+    print(statistics)
 
-    tor_stats()
+#print(f"Total Received: {gb_read} GB")
+#print(f"Total Sent:     {gb_written} GB")
+#print(f"Active Circuits: {connections}")
+#print(f"Uptime:         {uptime} seconds")
